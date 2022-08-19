85c18b8a5b822294d95a87b2d369c06885e576e8
ChanelT <crene7722@gmail.com>
2022-08-18 21:23:48 -0500

    convert to ejs

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const path = require('path')
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const exphbs = require("express-handlebars");
-const methodOverride = require('method-override')
-const passport = require("passport")
-const session = require("express-session")
-const MongoStore = require('connect-mongo')
+const express = require('express');
+const app = express();
 const connectDB = require("./config/db");
+const dotenv = require('dotenv');
+const methodOverride = require('method-override')
+const MongoStore = require('connect-mongo')
+
 
 //Load config
-dotenv.config({ path: "./config/config.env" });
-
-//Passport config
-require('./config/passport')(passport)
+dotenv.config({path: './config/config.env'});
 
 connectDB();
 
-const app = express();
-
+app.set('view engine', 'ejs');
+// Static folder
+app.use(express.static('public'))
 // Body parser
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 // Method override
@@ -34,54 +29,11 @@ app.use(
     }
 }))
 
-//Logging
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
-}
-
-// Handlebars Helpers
-const {formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
-
-// Handlebars
-//!Add the word .engine after exphbs
-app.engine(".hbs", exphbs.engine({
-    helpers: {
-        formatDate,
-        stripTags,
-        truncate,
-        editIcon,
-        select,
-    },   
-    defaultLayout: "main",
-    extname: ".hbs",
-    })
-);
-app.set("view engine", ".hbs");
-
-// Sessions
-app.use(
-    session({
-        secret: 'keyword cat',
-        resave: false,
-        saveUninitialized: false,
-        store: MongoStore.create({
-            mongoUrl: process.env.MONGO_URI
-        })
-    })
-)
-
-// Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
-
 // Set global var
 app.use(function (req, res, next) {
     res.locals.user = req.user || null
     next()
 })
-
-// Static folder
-app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use("/", require('./routes/index'));
