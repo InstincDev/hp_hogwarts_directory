b2d877608c1905ea6e8c0918513c2ea333bd9a36
ChanelT <crene7722@gmail.com>
2022-08-20 12:37:09 -0500

    added house dashboards

--- controllers/houses.js
+++ controllers/houses.js
@@ -5,7 +5,6 @@ module.exports = {
     getPerson: async (req, res) => {
         try{
             let person = await Character.findById(req.params.id).lean() 
-            console.log(typeof person)
             if(!person){
                 return res.render('errors/404')
             } 
@@ -29,7 +28,60 @@ module.exports = {
             console.error(err);
             res.render("errors/500");
         }
+    },
+
+    getGryffindors: async (req, res) => {
+        try{
+            let charData = await Character.find({house:"Gryffindor"}).lean() 
+            if(!charData){
+                return res.render('errors/404')
+            } 
+            res.render('houses/gryffindors',{charData})
+        } catch (err){
+              console.error(err)
+              res.render('errors/404')
+        }
+      
+  },
+    getSlytherins: async (req, res) => {
+        try{
+            let charData = await Character.find({house:"Slytherin"}).lean() 
+            if(!charData){
+                return res.render('errors/404')
+            } 
+            res.render('houses/slytherins',{charData})
+        } catch (err){
+            console.error(err)
+            res.render('errors/404')
+        }
+  
+    },
+    getHufflepuffs: async (req, res) => {
+    try{
+        let charData = await Character.find({house:"Hufflepuff"}).lean() 
+        if(!charData){
+            return res.render('errors/404')
+        } 
+        res.render('houses/hufflepuffs',{charData})
+    } catch (err){
+          console.error(err)
+          res.render('errors/404')
     }
+  
+},
+    getRavenclaws: async (req, res) => {
+        try{
+            let charData = await Character.find({house:"Ravenclaw"}).lean() 
+            if(!charData){
+                return res.render('errors/404')
+            } 
+            res.render('houses/ravenclaws',{charData})
+        } catch (err){
+              console.error(err)
+              res.render('errors/404')
+        }
+      
+  },
 
     
 }
--- routes/houses.js
+++ routes/houses.js
@@ -1,16 +1,29 @@
-const express = require('express')
-const router = express.Router()
-const housesController = require('../controllers/houses')
-
+const express = require("express");
+const router = express.Router();
+const housesController = require("../controllers/houses");
 
 // @desc    Show Person Page
 // @route   GET /houses/person
-router.get('/person/:id', housesController.getPerson)
+router.get("/person/:id", housesController.getPerson);
 
+// @desc    Show Gryffindors Page
+// @route   GET /houses/gryffindors
+router.get("/gryffindors", housesController.getGryffindors);
 
+// @desc    Show Slytherins Page
+// @route   GET /houses/slytherins
+router.get("/slytherins", housesController.getSlytherins);
+
+// @desc    Show Hufflepuffs Page
+// @route   GET /houses/hufflepuffs
+router.get("/hufflepuffs", housesController.getHufflepuffs);
+
+// @desc    Show Ravenclaws Page
+// @route   GET /houses/ravenclaws
+router.get("/ravenclaws", housesController.getRavenclaws);
 
 // @desc    Process add form
 // @route   POST /houses
-router.post("/", housesController.addPerson) 
+router.post("/", housesController.addPerson);
 
-module.exports = router
+module.exports = router;
